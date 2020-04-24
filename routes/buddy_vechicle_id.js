7117218b8b8268d372a8da0d8772f124907def2a
jinheeyi <jinhee.yi27@gmail.com>
2020-04-25 08:28:52 +1000

    Buddy Vehicle ID - Posting in MongDB

--- routes/buddy_vechicle_id.js
+++ routes/buddy_vechicle_id.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const router = express.Router();
+const buddyVehicleIdModel = require("../models/Buddy_Vehicle");
 
 //CRUD Create - Retrieve - Update - Delete //
 
@@ -13,19 +14,38 @@ router.get ('/', (req, res) => {
 //Develop an API for register the certain Buddy Vehicle's ID
 router.post ('/', (req, res) => {
     
-    const newBuddyVehicleId = {
-        buddyVehicleId: req.body.buddyVehicleId,
-        buddyId: req.body.buddyId,
-        vehicleType: req.body.vehicleType,
-        vehicleModel: req.body.vehicleModel,
-        vehicleColor: req.body.vehicleColor,
-        vehicleInsurance: req.body.vehicleInsurance
-    };
-
-    res.json ({
-        msg: "Registered new buddy vehicle's ID",
-        buddyVehicleId: newBuddyVehicleId
+    const newBuddyVehicleId = new buddyVehicleIdModel ({
+        buddy_vehicle_id: req.body.buddy_vehicle_id,
+        delivery_buddy_id: req.body.delivery_buddy_id,
+        vehicle_registration_number: req.body.vehicle_registration_number,
+        vehicle_type: req.body.vehicle_type,
+        vehicle_model: req.body.vehicle_model,
+        vehicle_color: req.body.vehicle_color,
+        vehicle_insurance: req.body.vehicle_insurance,
+        insurance_documents: req.body.insurance_documents
     });
+    //항상 기본적인 포맷이다. Save - Then - Catch API 호출..
+    newBuddyVehicleId
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Handling POST requests to Buddy Vehicle ID',
+                createBuddyIdInfo: result
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err.message
+            });
+        });
+
+
+
+
+    // res.json ({
+    //     msg: "Registered new buddy vehicle's ID",
+    //     buddyVehicleId: newBuddyVehicleId
+    // });
 });
 
 //API for patching 
